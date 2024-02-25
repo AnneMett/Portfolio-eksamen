697e417c1ecf11bbe0c8d76026893f86a4f4da5a
AM <126450800+AnneMett@users.noreply.github.com>
2024-02-25 17:09:50 +0100

    more stuff done

--- script.js
+++ script.js
@@ -3,9 +3,6 @@
 
 // her er JS til projekt tab - knapperne //
 
-
-
-
 document.addEventListener("DOMContentLoaded", function() {
 // Sætter den første fane som aktiv ved sidenbelastning
 // Finder den første faneknap og tilføjer "active" klassen for at markere den som aktiv
@@ -35,3 +32,42 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 });
+
+
+
+/* det her er til billedegallery på nippon projekt 
+ref: w3schools: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow */
+let slideIndex = 1; // Initialiserer variablen 'slideIndex' og sætter den til 1, hvilket repræsenterer det første slide.
+showSlides(slideIndex); // Kalder funktionen 'showSlides' for at vise det første slide.
+
+// Funktion til at ændre slides med en bestemt mængde
+function plusSlides(n) {
+  showSlides(slideIndex += n); // Kalder funktionen 'showSlides' med det nye slideindeks efter tilføjelse af 'n'.
+}
+
+// Funktion til at ændre slide til en bestemt position
+function currentSlide(n) {
+  showSlides(slideIndex = n); // Kalder funktionen 'showSlides' med det valgte slideindeks 'n'.
+}
+
+// Funktion til at vise slides
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides"); // Henter alle elementer med klassenavnet 'mySlides' (dine slides).
+  let dots = document.getElementsByClassName("dot"); // Henter alle elementer med klassenavnet 'dot' (dine punkter).
+  if (n > slides.length) {slideIndex = 1}    // Hvis 'n' er større end antallet af slides, skal 'slideIndex' sættes til 1 for at gå til det første slide.
+  if (n < 1) {slideIndex = slides.length} // Hvis 'n' er mindre end 1, skal 'slideIndex' sættes til det sidste slide.
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  // Skjuler alle slides.
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", ""); // Fjerner klassen 'active' fra alle punkter.
+  }
+  slides[slideIndex-1].style.display = "block";  // Viser det aktuelle slide ved at ændre dets stilattribut til 'display: block'.
+  dots[slideIndex-1].className += " active"; // Tilføjer klassen 'active' til det aktuelle punkt for at markere det som aktivt.
+}
+
+// Automatisk ændring af slides hvert 3. sekund (3000 millisekunder)
+setInterval(function() {
+  plusSlides(1); // Ændrer slide med 1
+}, 6000); // Intervallet er 6000 millisekunder (6 sekunder), kan ændres efter behov.
