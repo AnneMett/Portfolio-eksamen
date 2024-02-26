21c16dabcb35eefb59e032b6ab7ea51275a3fd26
AM <126450800+AnneMett@users.noreply.github.com>
2024-02-26 19:55:51 +0100

    ugh update
    i'm not gonna finish in time - not a product i'd be satisfied with at least...

--- script.js
+++ script.js
@@ -141,20 +141,3 @@ setInterval(function() {
 }, 6000);
 
 
-// til accordian elementet i about me sektionen 
-//ref w3schools: https://www.w3schools.com/howto/howto_js_accordion.asp 
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
-}
